573e01d92296f8a10d1ed5b799b07205f5472b95
Ale <callaandbandc@163.com>
2020-04-15 18:04:04 +0800

    1、修改了Blog的一些样式，顺便适配了手机端;2、新增了分页系统，后期再做封装整理;3、新增了style.css样式文件，用于全局的样式处理;

--- AleBlog.Web/Program.cs
+++ AleBlog.Web/Program.cs
@@ -21,7 +21,7 @@ namespace AleBlog.Web
             await Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(builder =>
             {
                 builder.ConfigureKestrel(options => { options.AddServerHeader = false; })
-                       .UseUrls("http://*:5001")
+                       .UseUrls("http://*:5003")
                        .UseStartup<Program>();
             }).Build().RunAsync();
         }
