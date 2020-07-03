78bfbe7ba76cad7327ed09605cb3a1d89530e3ac
Ale <callaandbandc@163.com>
2020-07-03 23:12:20 +0800

    同步文件夹内的md文件

---
+++ AleBlog.Sync/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace AleBlog.Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //同步文件夹内md文件到博客
+
+            
+        }
+    }
+}
